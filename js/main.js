f78d1464ea2606a891fc707f4a04825c4bfb5eff
Fede Camara Halac <camarafede@gmail.com>
2017-03-04 22:15:39 +0100

    $ARG

--- js/main.js
+++ js/main.js
@@ -40,3 +40,34 @@ function layoutSetup(w, h) {
 	//$(".soundcloud").width(content);
 	$(".image-window img").width(w * 0.2);
 }
+
+
+//TODO for work
+$(".duotri").click(function() {
+$(".multimedia, .ensemble, .solo, .videoart, .orchestra, .inprogress").fadeOut(300);
+$(".duotrio").fadeIn(300);
+});
+$(".inprogresso").click(function() {
+$(".multimedia, .ensemble, .solo, .videoart, .orchestra, .duotrio").fadeOut(300);
+$(".inprogress").fadeIn(300);
+});
+$(".multimedi").click(function() {
+$(".ensemble, .solo, .videoart, .orchestra, .duotrio, .inprogress").fadeOut(300);
+$(".multimedia").fadeIn(300);
+});
+$(".videoar").click(function() {
+$(".multimedia, .ensemble, .solo, .orchestra, .duotrio, .inprogress").fadeOut(300);
+$(".videoart").fadeIn(300);
+});
+$(".sol").click(function() {
+$(".multimedia, .ensemble, .videoart, .orchestra, .duotrio, .inprogress").fadeOut(300);
+$(".solo").fadeIn(300);
+});
+$(".ensembl").click(function() {
+$(".multimedia, .solo, .videoart, .orchestra, .duotrio, .inprogress").fadeOut(300);
+$(".ensemble").fadeIn(300);
+});
+$(".orchestr").click(function() {
+$(".multimedia, .ensemble, .solo, .videoart, .duotrio, .inprogress").fadeOut(300);
+$(".orchestra").fadeIn(300);
+});
