ab12b4988e0277975e041a5574fdaec6ffadc0da
Fede Camara Halac <camarafede@gmail.com>
2017-03-04 09:58:11 +0100

    added images

--- js/main.js
+++ js/main.js
@@ -1,54 +1,47 @@
 //Get a random number to be margin
 function getRandomMargin() {
-  var margen = (5 * Math.random()) + 3;
-  return margen;
+var margen = (5 * Math.random()) + 3;
+return margen;
 }
 ///Make Random Margins 
 function randomMargenMotionL() {
-  $(".menulink-left").delay(4333).animate({
-    marginLeft: (getRandomMargin() * -0.5) //up to half of the right margin
-  }, 4333);
+$(".menulink-left").delay(4333).animate({
+marginLeft: (getRandomMargin() * -0.5) //up to half of the right margin
+}, 4333);
 }
 function randomMargenMotionR() {
-  $(".menulink").delay(4333).animate({
-    marginRight: getRandomMargin()
-  }, 4333);
+$(".menulink").delay(4333).animate({
+marginRight: getRandomMargin()
+}, 4333);
 }
 function randomMargenWorks() {
-  $(".nums").delay(4333).animate({
-    marginRight: getRandomMargin()*0.5
-  }, 4333*1.3);
+$(".nums").delay(4333).animate({
+marginRight: getRandomMargin()*0.5
+}, 4333*1.3);
 }
-//Set  Random Margens going every 3000
+//SetRandom Margens going every 3000
 setInterval(function() {
-  randomMargenMotionL();
+randomMargenMotionL();
 }, 4333);
 setInterval(function() {
-  randomMargenMotionR();
+randomMargenMotionR();
 }, 4333);
 setInterval(function() {
-  randomMargenWorks();
+randomMargenWorks();
 }, 4333);
 //main function on startup
 $(function() {
-  var w = $(window).width();
-  var h = $(window).height();
-  var content = w * 0.9;
-  $("#header").load("menu.html");
-  $("#content").width(content).load("content/upcoming.html").delay(500).animate({opacity: 1}, 500);
-//adjusting margins	
-  $(".score-object").width(content).height(content * 0.7);
-  $(".audio-object").width(content);
-  $("#content img").width(w * 0.25).css("margin", 0);
-  $(".video-iframe").width(content).css("margin", 0).css("padding", 0).height(content * 0.4);
-  $(".audio-iframe").width(content).css("margin", 0).css("padding", 0).height(content * 0.15);
-  $(".soundcloud").width(content);
-  $(".blog").width(content).height(content);
-  $(".image-window img").width(w * 0.2);
-  $("#english").click(function() {
-    $(".spa").hide(), $(".eng").show()
-  });
-  $("#spanish").click(function() {
-    $(".eng").hide(), $(".spa").show()
-  });
+var w = $(window).width();
+var h = $(window).height();
+var content = w * 0.9;
+$("#header").load("menu.html");
+$("#content").width(content).load("content/upcoming.html").delay(500).animate({opacity: 1}, 500);
+//adjusting margins
+$(".score-object").width(content).height(content * 0.7);
+$(".audio-object").width(content);
+$("#content img").width(w * 0.25).css("margin", 0);
+$(".video-iframe").width(content).css("margin", 0).css("padding", 0).height(content * 0.4);
+$(".audio-iframe").width(content).css("margin", 0).css("padding", 0).height(content * 0.15);
+$(".soundcloud").width(content);
+$(".image-window img").width(w * 0.2);
 });
