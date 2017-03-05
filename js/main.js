29c5008e2356cd6c235491b0138d715eec943620
Fede Camara Halac <camarafede@gmail.com>
2017-03-05 02:08:05 +0100

    $ARG

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ $(function() {
 	
 	languagesMenu();
 	layoutSetup(w, h);
-	
-	
+	readTextFile("ytinfo", function(text) {
+		var data = JSON.parse(text);
+		console.log(data);
+	});
 });
 
 //loads the id of the element passed on the content div 
@@ -33,7 +35,17 @@ function languagesMenu() {
 	});
 }
 
-
+function readTextFile(file, callback) {
+	var rawFile = new XMLHttpRequest();
+	rawFile.overrideMimeType("application/json");
+	rawFile.open("GET", file, true);
+	rawFile.onreadystatechange = function() {
+		if (rawFile.readyState === 4 && rawFile.status == "200" ) {
+			callback(rawFile.responseText);;
+		}
+	}
+	rawFile.send(null);
+}
 
 
 //reset the layout
