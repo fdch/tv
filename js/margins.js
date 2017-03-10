01e781f6822dc1f6f2b1ec1c8bfea93a69c168e5
Fede Camara Halac <camarafede@gmail.com>
2017-03-11 00:15:53 +0100

    $1

--- js/margins.js
+++ js/margins.js
@@ -4,32 +4,16 @@ function getRandomMargin() {
 	return margen;
 }
 ///Make Random Margins 
-//function randomMargenMotionL() {
-//	$(".menulink-left").delay(4333).animate({
-//		marginLeft: (getRandomMargin() * -0.5) //up to half of the right margin
-//		}, 4333);
-//}
 function randomMargenMotionR() {
 	$(".menulink").delay(4333).animate({
 		marginRight: getRandomMargin()
 	}, 4333);
 }
-function randomMargenWorks() {
-	$(".nums").delay(4333).animate({
-		marginRight: getRandomMargin()*0.5
-	}, 4333*1.3);
-}
+
 //SetRandom Margens going every 3000
-setInterval(function() {
-	randomMargenMotionL();
-}, 4333);
 
 setInterval(function() {
 	randomMargenMotionR();
-}, 4333);
-
-setInterval(function() {
-	randomMargenWorks();
 }, 4333);
 
 //Every 5000, change the opacity of the fd image
