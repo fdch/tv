b31c404a064133ecdff4db52f62625d690810d8d
Fede Camara Halac <camarafede@gmail.com>
2017-06-20 17:49:25 -0400

    updates

--- js/config.js
+++ js/config.js
@@ -10,13 +10,12 @@ var facebookUrl = "https://www.facebook.com/fedecamarahalac/";
 var title = "Multimedianautics";
 var subtitle = "Fede Camara Halac";
 var featURL = "https://player.vimeo.com/video/168692629";
-var featured =  [featURL, ";
 var bioOpen = "<article><a href=\"img/fdch.jpg\"><img src=\"img/fdch.jpg\" title=\"[ph] aye savid\"width=200></a><h4>curriculum vitae (<a href=\"cv/\" target=\"_blank\">html</a>, <a href=\"cv/pdf/cv.pdf\" target=\"_blank\">pdf</a>)</h4>";
 var bioClose = "</article>";
 ////////////
 ////Title
 ////////////
-var titleData = "<h1 onclick=\"location.href='"+ url + "'\">"+ title +"</h1><h2 onclick=\"location.href='" + url + "'\">" + subtitle + "</h2><iframe src=\"" + featURL+ "\" height="+ logoimage[1] +"/></div><div id=menu></div><div id=submenu></div><div id=content></div>";
+var titleData = "<h1 onclick=\"location.href='"+ url + "'\">"+ title +"</h1><h2 onclick=\"location.href='" + url + "'\">" + subtitle + "</h2><div id=menu></div><div id=submenu></div><div id=content></div>";
 ////////////
 //// Meta properties
 ////////////
