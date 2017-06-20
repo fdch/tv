883cd078ecba7760695c5ff180647075f4556eef
Fede Camara Halac <camarafede@gmail.com>
2017-06-20 17:27:24 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ function loader(x) {
       break;
     case "content/bio":
       $("#submenu").html("");
-      $("#content")html("").append([bioOpen, "<p>"+bioData+"</p>", bioClose]);
+      $("#content").html("").append([bioOpen, "<p>"+bioData+"</p>", bioClose]);
       break;
     default:
       $("#submenu").html("");
