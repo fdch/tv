3cdea851d22a05315049379c10e9c4ce95220021
Fede Camara Halac <camarafede@gmail.com>
2017-06-20 18:00:04 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ function randomMargin(t) {
   $(".menuitem").animate({marginRigh:5 * Math.random() + 3},t);
 }
 
-var video = "<video class=backvideo src=\'"+featURL+"\' autoplay />";
+var video = "<video class=backvideo autoplay loop muted /><source src=\'"+featURL+"\' type=\"video/mp4\"> </video>";
 
 
 $(function(){
