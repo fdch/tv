2dfa4a76aa1c344d5ae324e70ee5b02b04e4b8c9
Fede Camara Halac <camarafede@gmail.com>
2017-06-20 14:02:50 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -77,14 +77,26 @@ var w, h;
 var mitem = ["bio", "work", "social", "games", "blog", "video", "code", "contact"];
 var lang = "<div id=languages><span class=menulink-left><span id=english ><spa class=eng>[</spa>en<spa class=eng>]</spa></span>&nbsp;|&nbsp;<span id=spanish ><spa class=spa >[</spa>es<spa class=spa >]</span></span></div>"
 //<img class=fd src="img/fd.png" title=fd/></img>
-
 function makeMenu(m, len, mitem, type) {
   var i,j;
   m.append("<nav>");
   for (i = 0;i < len; i++) {
-    m.append("<"+type+" class=menuitem onClick=\"get" + mitem[i].substring(0,6) + "(" + mitem[i].toLowerCase().replace(/_/g,"") + ")\">  " + mitem[i].replace(/_/g," ") + "  </"+type+">");
+    m.append("<"+type+" class=menuitem onClick=\"loader(" + mitem[i] + ")\"></"+type+">");
   }
   m.append("</nav>");
+}
+
+function loader(x) {
+  switch (x) {
+    case "blog" : window.open(blogurl);
+    break;
+    case "video" : window.open(videourl);;
+    break;
+    case "code" : window.open(repo); ;
+    break;
+    default:
+     document.getElementById('content').load(x);
+  }
 }
 
 $(function(){
