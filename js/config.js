dc17a6b718ebd4cdc8668b54cce958c04b24bbcd
Fede Camara Halac <camarafede@gmail.com>
2018-07-05 12:30:04 +0200

    updates

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var w, h, t = 4333;
-var mitem = ["bio", "work", "events", "social", "games", "contact", "blog", "video", "fdch"];
+var mitem = ["bio", "work", "events", "social", "games", "contact"];
 
 var url = "https://fdch.github.io/tv";
 var repo = "https://github.com/fdch/";
--- js/main.js
+++ js/main.js
@@ -33,7 +33,8 @@ $(function(){
   $("footer").append([
     linkify("blog",blog,1),
     linkify("code",repo,1),
-    linkify("video",video,1)
+    linkify("video",video,1),
+    linkify("audio",scloud,1)
     ]);
 });
 /////////////////////////////////////// main
