65eb29e0d249777198f15462f45974b2b2051c48
Fede Camara Halac <camarafede@gmail.com>
2018-07-07 13:58:34 +0200

    updates

--- js/cv_main.js
+++ js/cv_main.js
@@ -19,12 +19,12 @@ $(function(){
   $("article").width(width()).height(height());
 
   $("main article").append([
-  	"<section id=personal>"+tag("header",tag("h4","Personal Information"))+"</section>",
-  	"<section id=teachings>"+tag("header",tag("h4","Teaching Experience"))+"</section>",
-  	"<section id=awards>"+tag("header",tag("h4","Awards"))+"</section>",
-  	"<section id=unworks>"+tag("header",tag("h4","List of unworks"))+"</section>",
-  	"<section id=collaborations>"+tag("header",tag("h4","Collaborations"))+"</section>",
-  	"<section id=performances>"+tag("header",tag("h4","Performances"))+"</section>"
+  	"<section id=personal>"       + tag("header",tag("h4","Personal Information"))+"</section>",
+  	"<section id=teachings>"      + tag("header",tag("h4","Teaching Experience")) +"</section>",
+  	"<section id=awards>"         + tag("header",tag("h4","Awards"))              +"</section>",
+  	"<section id=unworks>"        + tag("header",tag("h4","List of unworks"))     +"</section>",
+  	"<section id=collaborations>" + tag("header",tag("h4","Collaborations"))      +"</section>",
+  	"<section id=performances>"   + tag("header",tag("h4","Performances"))        +"</section>"
   	])
 
   $("#personal").append(tag("ul",tag("li",personal.join("</li><li>"))));
