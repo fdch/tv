abdab6c6f1e35efcdda5413c5dc42ab664d13061
Fede Camara Halac <camarafede@gmail.com>
2018-07-07 23:15:03 +0200

    updates

--- js/cv_main.js
+++ js/cv_main.js
@@ -11,26 +11,24 @@ $(function(){
   
   randomColor(color_preset["mid"]); 
   
-  $("h1, h2").click(function() {
-    window.open(cvurl, "_top")
-  });
+  $("h1, h2").click(function() {window.open(cvurl, "_top")});
 
   $("main").html("").append("<article></article>");
-
   $("article, #menubg").width(articleWidth(maxWidth));
 
   var allsections = ["Personal", "Teachings", "Awards", "Unworks", "Collaborations", "Performances"];
-
-  for (var i in allsections) {
-    var theid = makeID(allsections[i]);
-    var func = "get"+allsections[i];
-    $("main article").append(
+  $.each(allsections, function(index,value){
+    var theid = makeID(value);    $("main article").append(
       "<section id="+theid+
       "<header onclick=\"function(){$(\'#"+theid+" > ul\').toggle()}\">"+
       "</section>");
-    // console.log(func);
-    window[func]();
-  }
+  })
+  
+  getPersonal();
+  getTeachings();
+  getAwards();
+  getUnworks();
+  getCollaborations();
+  getPerformances();
 
-  $("footer").append(linkify("back~", url));
-});
+  $("footer").append(linkify("back~", url));});
