16eb87583973f46afb639b58ac87560af4f7d41a
Fede Camara Halac <camarafede@gmail.com>
2018-07-08 12:36:08 +0200

    updates

--- js/config.js
+++ js/config.js
@@ -180,11 +180,11 @@ var imgArray = ["img/imgone.png","img/imgtwo.png"];
 
 
 var formMenu = [
-"<h3 "+onclickify("window.open",formL[0]+forms[allCVsections[1]]+formL[1])+">"+allCVsections[1]+"</h3>",
-"<h3 "+onclickify("window.open",formL[0]+forms[allCVsections[2]]+formL[1])+">"+allCVsections[2]+"</h3>",
-"<h3 "+onclickify("window.open",formL[0]+forms[allCVsections[3]]+formL[1])+">"+allCVsections[3]+"</h3>",
-"<h3 "+onclickify("window.open",formL[0]+forms[allCVsections[4]]+formL[1])+">"+allCVsections[4]+"</h3>",
-"<h3 "+onclickify("window.open",formL[0]+forms[allCVsections[5]]+formL[1])+">"+allCVsections[5]+"</h3>",
+"<span class=formlink  "+onclickify("window.open",formL[0]+forms[allCVsections[1]]+formL[1])+">"+allCVsections[1]+"</span>",
+"<span class=formlink  "+onclickify("window.open",formL[0]+forms[allCVsections[2]]+formL[1])+">"+allCVsections[2]+"</span>",
+"<span class=formlink  "+onclickify("window.open",formL[0]+forms[allCVsections[3]]+formL[1])+">"+allCVsections[3]+"</span>",
+"<span class=formlink  "+onclickify("window.open",formL[0]+forms[allCVsections[4]]+formL[1])+">"+allCVsections[4]+"</span>",
+"<span class=formlink  "+onclickify("window.open",formL[0]+forms[allCVsections[5]]+formL[1])+">"+allCVsections[5]+"</span>",
 "<h3 "+onclickify("window.open",formL[0]+forms[webSections[0]]  +formL[1])+">"+webSections[0]  +"</h3>",
 "<h3 "+onclickify("window.open",formL[0]+forms[webSections[1]]  +formL[1])+">"+webSections[1]  +"</h3>",
 "<h3 "+onclickify("window.open",formL[0]+forms[webSections[2]]  +formL[1])+">"+webSections[2]  +"</h3>"
