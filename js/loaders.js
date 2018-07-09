48aa8ee9cddf97bbd598681394eb3a6b6bb7fc43
Fede Camara Halac <camarafede@gmail.com>
2018-07-09 17:07:58 +0200

    updates

--- js/loaders.js
+++ js/loaders.js
@@ -109,7 +109,7 @@ function getUnworks() {
     var formAttId = document.createAttribute('id');
     formAttId.value = formID;
 
-    $("#submenu").appendChild(formTag);
+    document.body.getElementById("submenu").appendChild(formTag);
     formTag.setAttributeNode(formAttId);
 
 
