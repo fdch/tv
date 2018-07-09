a47d4db08db962b509846bba30999fea1111d754
Fede Camara Halac <camarafede@gmail.com>
2018-07-09 18:34:23 +0200

    updates

--- js/routines.js
+++ js/routines.js
@@ -181,7 +181,7 @@ function makeValue(input,target) {
   elemTag.appendChild(elemTxt);
 }
 
-function getValue(this) {
-  var value = makeID(this.value);
+function getValue(x) {
+  var value = makeID(x.value);
   window.alert(value);
 }
