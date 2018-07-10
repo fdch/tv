04f1cf80e2e18dfa1b96b51f0383c1021deccebe
Fede Camara Halac <camarafede@gmail.com>
2018-07-10 16:02:52 +0200

    updates

--- js/routines.js
+++ js/routines.js
@@ -132,7 +132,7 @@ var whichone = 0;
 function funImage(input,target){
   dur = Math.random()*5000;
   ang = Math.random()*360*2-360;
-  input.rotate({animateTo: ang,duration: dur});
+  // input.rotate({animateTo: ang,duration: dur});
   if (whichone==0) {whichone=1} else {whichone=0}
   setTimeout(function(){
     input.attr('src', target[whichone])
