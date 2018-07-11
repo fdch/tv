e9d72cbdf04d51bd29686548a9717034eec80e8e
Fede Camara Halac <camarafede@gmail.com>
2018-07-11 14:59:46 +0200

    massive_javascript_turnaround

--- js/5-main.js
+++ js/5-main.js
@@ -10,30 +10,26 @@
 $(function(){
   htmlTag = document.getElementsByTagName('html')[0];
   randomFont(htmlTag);  
+  mainTag = document.getElementsByTagName('main')[0];
   bodyTag = document.getElementsByTagName('body')[0];
-  makeBackVideo("backvideo", bodyTag, featURL[pdRandom(2)]);
   headerTag = document.getElementsByTagName('header');
-  var h1Tag = document.getElementsByTagName('h1')[0];
   footerTag = document.getElementsByTagName('footer')[0];
+  footerTag.setAttribute('style', 'display:none');  
+
+  var h1Tag = document.getElementsByTagName('h1')[0];
+  h1Tag.setAttribute('onclick','window.open(\'url\', \'_top\');');
+  
+  makeBackVideo("backvideo", bodyTag, featURL[pdRandom(2)]);
   var rotImgTag = makeRot(rotImg, bodyTag);
   navTag = makeMenu(mitem, headerTag[0] , "display");
+
   randomColor(color_preset["high"], [bodyTag,headerTag[0]]);
-  var footerDisplay = document.createAttribute('style');
-  footerDisplay.value = "display:none";
-  footerTag.setAttributeNode(footerDisplay);  
+
   setInterval( function(){ funImage(rotImg) }, t);
-  var h1Click = document.createAttribute('onclick');
-  h1Click.value = "window.open(\'"+url+"\', \'_top\');";
-  h1Tag.setAttributeNode(h1Click);
-  mainTag = document.getElementsByTagName('main')[0];
+ 
   if (!loading) 
     while (!loaded) { 
     if (loaded==0) loaded = loadAll(allGS);
       else if (loaded==1) break; 
     }
-
-
-
-
-
 });
