36955aee749c82aff44a08c4b92081d7798ce4e6
Fede Camara Halac <camarafede@gmail.com>
2018-07-11 16:54:20 +0200

    massive_javascript_turnaround

--- js/0-rout.js
+++ js/0-rout.js
@@ -207,7 +207,7 @@ function getSubmit(target) {
       n.alert("\nSucess!\n\n"+submit[1]+"\n\n Karlheinz.");
       n.resizeTo(articleWidth(maxWidth), height());
       n.moveBy(articleWidth(maxWidth)*0.3, 0);
-      open(formLinks[String(target)], "editForm");
+      open(String(formLinks[String(target)]), "editForm");
     }
   }
 }
