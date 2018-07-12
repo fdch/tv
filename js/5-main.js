b317a6e3c58cad5d1f1801c25d60f173d7b8dae3
Fede Camara Halac <camarafede@gmail.com>
2018-07-12 16:15:51 +0200

    massive_javascript_turnaround

--- js/5-main.js
+++ js/5-main.js
@@ -23,7 +23,7 @@ function main() {
   
   if(!navigaTag) navigaTag = makeMenu(mitem, headerTag[0] , "display");
   if(!iframeTag) iframeTag = makeBackVideo("backvideo", bodyTag, featURL[pdRandom(2)]);
-  if(!rotImgTag) rotImgTag = makeRot(rotImg, headerTag[0], w*0.25);
+  if(!rotImgTag) rotImgTag = makeRot(rotImg, headerTag[0], w*h*0.125);
   
   randomColor(color_preset["high"], [bodyTag, headerTag[0]] );
 
