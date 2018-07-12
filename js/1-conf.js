8032738c16959d5ab1efd537ec39dbdc799eed34
Fede Camara Halac <camarafede@gmail.com>
2018-07-12 18:07:33 +0200

    massive_javascript_turnaround

--- js/1-conf.js
+++ js/1-conf.js
@@ -20,6 +20,7 @@ var w, h;
 var t = 4333;
 var maxWidth = 810;
 var widthFactor = 0.9;
+var headSiz = [maxWidth,30]; //header max size, rotIm init size
 
 var url = "https://fdch.github.io/tv";
 var repo = "https://github.com/fdch/";
