e99bd16106f172e4f6ff3dcefaa199b87e09e9e8
Fede Camara Halac <camarafede@gmail.com>
2018-07-12 13:20:12 +0200

    massive_javascript_turnaround

--- js/5-main.js
+++ js/5-main.js
@@ -8,23 +8,22 @@
 ///           ////////              \\\       ////       ///
 //           ///////                  \\\     ////       ///
 function main() {
-  if(! (htmlTag   = document.getElementsByTagName('html')[0]) );
+  if(!htmlTag)   htmlTag   = document.getElementsByTagName('html')[0];
   randomFont(htmlTag);  
-  
-  if(! (mainTag   = document.getElementsByTagName('main')[0]) );
-  if(! (bodyTag   = document.getElementsByTagName('body')[0]) );
-  if(! (headerTag = document.getElementsByTagName('header'))  );
-  if(! (footerTag = document.getElementsByTagName('footer')[0]) );
+  if(!bodyTag)   bodyTag   = document.getElementsByTagName('body')[0];
+  if(!headerTag) headerTag = document.getElementsByTagName('header');
+  if(!mainTag)   mainTag   = document.getElementsByTagName('main')[0];
+  if(!footerTag) footerTag = document.getElementsByTagName('footer')[0];
   footerTag.setAttribute('style', 'display:none');  
 
-  if(! (h1titlTag = document.getElementsByTagName('h1')[0]) );
+  if(!h1titlTag) h1titlTag = document.getElementsByTagName('h1')[0];
   h1titlTag.setAttribute('onclick',"display(\'init\');");
   
-  if(! (navigaTag = makeMenu(mitem, headerTag[0] , "display")) );
-  if(! (iframeTag = makeBackVideo("backvideo", bodyTag, featURL[pdRandom(2)])) );
-  if(! (rotImgTag = makeRot(rotImg, bodyTag)) );
+  if(!navigaTag) navigaTag = makeMenu(mitem, headerTag[0] , "display");
+  if(!iframeTag) iframeTag = makeBackVideo("backvideo", bodyTag, featURL[pdRandom(2)]);
+  if(!rotImgTag) rotImgTag = makeRot(rotImg, bodyTag);
   
-  randomColor(color_preset["high"], [bodyTag,headerTag[0]]);
+  randomColor(color_preset["high"], [bodyTag, headerTag[0]] );
 
   setInterval( function(){ funImage(rotImg) }, t);
  
