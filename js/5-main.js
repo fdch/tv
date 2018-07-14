ee16a4d7bd2f66949c891d1c66ff339c219bd272
Fede Camara Halac <camarafede@gmail.com>
2018-07-14 23:05:38 +0200

    massive_javascript_turnaround

--- js/5-main.js
+++ js/5-main.js
@@ -1,11 +1,13 @@
-///////////////////////////\\\\\\\\\    ////////////////////  \\\\\\\\\\    //
-//////           //////////        \\   //////////            //////////    //
-//////           //////////         \\  //////////            //////////    //
-//////////////   //////////         //  //////////            ////////////////
-//////           //////////        //   //////////            //////////    //
-//////           //////////       //    //////////            //////////    // 
-//////           //////////////////     ////////////////////  //////////    //
+///////////////////////////\\\\\\\\\  ////////////////////  \\\\\\\\\\    ///
+//////           //////////        \\ //////////            //////////    ///
+//////           //////////         \\//////////            //////////    ///
+//////////////   //////////         ////////////            /////////////////
+//////           //////////        // //////////            //////////    ///
+//////           //////////       //  //////////            //////////    /// 
+//////           //////////////////   ////////////////////  //////////    ///
 function main() {
+  userLang = navigator.language || navigator.userLanguage; 
+  //console.log("The language is: " + userLang);
   if(!htmlTag)  htmlTag  =document.getElementsByTagName('html')[0];
   htmlTag.style.fontFamily = fonts[2];
   if(!headerTag)headerTag=document.getElementsByTagName('header');
@@ -18,13 +20,10 @@ function main() {
   if(!iframeTag){
     iframeTag=makeBackVideo("bkvid",bodyTag,featURL[pdRandom(2)],backImg);
   }
-  
   randomColor(color_preset["high"], [bodyTag, headerTag[0]] );
   setInterval( function(){ funImage(rotImg) }, t);
-
   if(!h1titlTag)h1titlTag=document.getElementsByTagName('h1')[0];
   h1titlTag.setAttribute('onclick',"display(\'init\');");
-
   
   if(!navigaTag) {
     navigaTag = document.getElementById('navtag');
@@ -32,17 +31,13 @@ function main() {
     navSelTag = makeDropdowns("navseltag", navigaTag, mitem, 'display(this)');
     navSelTag.autofocus = true;
   }
-
   //if(!navigaTag)navigaTag=makeMenu(mitem, headerTag[0] , "display");
   imgDivTag = document.getElementById('imgdiv');
   if(!rotImgTag)rotImgTag=makeRot(rotImg, imgDivTag);
-  
-  loadAll(allGS);
 
+  loadAll(allGS);
   if(!footerTag)footerTag=document.getElementsByTagName('footer')[0];
   footerTag.setAttribute('style', 'display:none');
   resized();
   iframeTag.style.background = '';  
-  userLang = navigator.language || navigator.userLanguage; 
-  console.log("The language is: " + userLang);
 }
